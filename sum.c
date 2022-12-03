db5d31f655671e2a4eac6c766fb881285a726050
fraxea <moh788672@gmail.com>
2022-12-03 21:59:49 +0330

    Update sum.c

--- sum.c
+++ sum.c
@@ -5,6 +5,7 @@ int main()
 {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 	//hola como estas?
 	/*
 	bien grasias
@@ -15,6 +16,11 @@ int main()
 	a -= 10;
 	b += 8;
 	printf("%i\n", a + b);
+=======
+	int a;
+	scanf("%i", &a);
+	printf("%i\n", a);
+>>>>>>> parent of bdb6dd9 (salam in dovomishe)
 =======
 	int a;
 	scanf("%i", &a);
